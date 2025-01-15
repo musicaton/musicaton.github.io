4aebbd655c5d6cd9a42987ddb7f92f3a37ebbba2
liviaalmeida <leave.ah@gmail.com>
2025-01-15 13:01:24 +0100

    New build

---
+++ assets/pause-BSabgeVL.js
@@ -0,0 +1,1 @@
+import{o as r,c as o,a as t}from"./index-BZzL00-o.js";const l={"xml:space":"preserve",width:"512",height:"512",fill:"currentColor",viewBox:"0 0 32 32"};function n(s,e){return r(),o("svg",l,e[0]||(e[0]=[t("rect",{width:"10",height:"28",x:"4",y:"2",rx:"3"},null,-1),t("rect",{width:"10",height:"28",x:"18",y:"2",rx:"3"},null,-1)]))}const i={render:n};export{i as default,n as render};
