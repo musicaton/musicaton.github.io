2af10734b06a5bcf927bd4b8a7771afdfe90f548
Lívia Almeida <leave.ah@gmail.com>
2025-05-21 18:48:59 +0200

    Add game of life rules

---
+++ assets/pause-biDMpLCk.js
@@ -0,0 +1,1 @@
+import{c as r,o,a as t}from"./index-DZwFNM7R.js";const l={"xml:space":"preserve",width:"512",height:"512",fill:"currentColor",viewBox:"0 0 32 32"};function n(s,e){return o(),r("svg",l,e[0]||(e[0]=[t("rect",{width:"10",height:"28",x:"4",y:"2",rx:"3"},null,-1),t("rect",{width:"10",height:"28",x:"18",y:"2",rx:"3"},null,-1)]))}const i={render:n};export{i as default,n as render};
